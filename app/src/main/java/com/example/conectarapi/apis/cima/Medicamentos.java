daac50afddbf4015d376c47b17512f4f8a111fce
ccaballero1504 <carloscaballerozamora9@gmail.com>
2026-05-11 14:05:00 +0200

    1.1

--- app/src/main/java/com/example/conectarapi/apis/cima/Medicamentos.java
+++ app/src/main/java/com/example/conectarapi/apis/cima/Medicamentos.java
@@ -1,14 +1,18 @@
 package com.example.conectarapi.apis.cima;
 
+import java.util.List;
+
 public class Medicamentos {
 
     private String nregistro, nombre;
-    boolean comerc;
+    private boolean comerc;
+    private List<Foto> fotos;
 
-    public Medicamentos(String nRegistro, String nombre, boolean comerc) {
+    public Medicamentos(String nRegistro, String nombre, boolean comerc, List<Foto> fotos) {
         this.nregistro = nRegistro;
         this.nombre = nombre;
         this.comerc = comerc;
+        this.fotos = fotos;
     }
 
     public String getnRegistro() {
@@ -19,5 +23,8 @@ public class Medicamentos {
     }
     public boolean isComerc() {
         return comerc;
+    }
+    public List<Foto> getFotos() {
+        return fotos;
     }
 }
